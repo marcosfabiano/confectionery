714e8883427772c2c06f52dbec1d66060507c5f5
Marcos Fabiano <marcos.fabiano67@outlook.com>
2023-11-29 17:40:18 -0300

    add frontend

---
+++ frontend/src/components/ButtonNextPage/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+type Props = {
+  onNextPage: Function;
+};
+
+export default function ButtonNextPage({ onNextPage }: Props) {
+  return (
+    <div onClick={() => onNextPage()} className="dsc-btn-next-page">
+      Carregar mais
+    </div>
+  );
+}
---
+++ frontend/src/services/product-service.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { ProductDTO } from "../models/product";
+import { requestBackend } from "../utils/requests";
+import { BASE_URL } from "../utils/system";
+
+export function findPageRequest(
+  page: number,
+  name: string,
+  productType: string,
+  size = 12,
+  sort = "name"
+) {
+  const config: AxiosRequestConfig = {
+    method: "GET",
+    url: "/products",
+    params: {
+      page,
+      name,
+      productType,
+      size,
+      sort,
+    },
+  };
+  return requestBackend(config);
+}
+
+export function findById(id: number) {
+  return requestBackend({ url: `/products/${id}` });
+}
+
+export function deleteById(id: number) {
+  const config: AxiosRequestConfig = {
+    method: "DELETE",
+    url: `/products/${id}`,
+    withCredentials: true,
+  };
+  return requestBackend(config);
+}
+
+export function updateRequest(obj: ProductDTO) {
+  const config: AxiosRequestConfig = {
+    method: "PUT",
+    url: `/products/${obj.id}`,
+    withCredentials: true,
+    data: obj,
+  };
+  return requestBackend(config);
+}
+
+export function insertRequest(obj: ProductDTO) {
+  const config: AxiosRequestConfig = {
+    method: "POST",
+    url: "/products",
+    withCredentials: true,
+    data: obj,
+  };
+  return requestBackend(config);
+}
